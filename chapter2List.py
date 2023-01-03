b081ea7fd61c1c4888cbf8cf0536651fa110a7b4
dlorenzo2000 <deanlorenzo@gmail.com>
2023-01-03 15:20:22 -0600

    added file to work with lists

---
+++ chapter2List.py
@@ -0,0 +1,11 @@
+# Date: Tue Jan03/23
+# Created by: Dean Lorenzo
+# Purpose: Putting lists to work
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+word = "Milliways"
+
+for letter in word:
+    if letter in vowels:
+        print(letter)
